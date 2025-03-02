c43e60a4cab18f275a328e38bd535bbfe9e35313
shisukeUrahara <abhisheksagu@gmail.com>
2025-03-02 13:36:52 +0530

    added helper config for anvil and eth-sepolia chain

--- 2_foundry_fund_me/script/DeployFundMe.s.sol
+++ 2_foundry_fund_me/script/DeployFundMe.s.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-
+import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployFundMe is Script {
     function deployFundMe() public returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig
+            .getConfigByChainId(block.chainid)
+            .priceFeed;
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
         return (fundMe);
     }
