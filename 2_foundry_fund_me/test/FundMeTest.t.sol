955ea317251f14a1cffb9025d512016bbc904181
shisukeUrahara <abhisheksagu@gmail.com>
2025-03-02 14:43:19 +0530

    updated test cases

--- 2_foundry_fund_me/test/FundMeTest.t.sol
+++ 2_foundry_fund_me/test/FundMeTest.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.18;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "../src/PriceConverter.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
@@ -12,10 +14,14 @@ contract FundMeTest is Test {
     address USER = makeAddr("user");
     address ANOTHER_USER = makeAddr("anotherUser");
 
+    uint256 public constant SEND_VALUE = 0.1 ether; // just a value to make sure we are sending enough!
+    uint256 public constant STARTING_BALANCE = 10 ether;
+    uint256 public constant GAS_PRICE = 1;
+
     // Set initial balance for test users
-    uint256 constant SEND_VALUE = 0.1 ether;
-    uint256 constant STARTING_BALANCE = 10 ether;
-    uint256 constant GAS_PRICE = 1;
+    // uint256 constant SEND_VALUE = 0.1 ether;
+    // uint256 constant STARTING_BALANCE = 10 ether;
+    // uint256 constant GAS_PRICE = 1;
 
     function setUp() external {
         // Deploy the contract directly instead of using DeployFundMe
@@ -37,11 +43,168 @@ contract FundMeTest is Test {
 
     // Test if the owner is set correctly
     function testOwnerIsDeployer() public view {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public view {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    // https://twitter.com/PaulRBerg/status/1624763320539525121
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        assert(address(fundMe).balance > 0);
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(address(3)); // Not the owner
+        fundMe.withdraw();
+    }
+
+    function testWithdrawFromASingleFunder() public funded {
+        // Arrange
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        // vm.txGasPrice(GAS_PRICE);
+        // uint256 gasStart = gasleft();
+        // // Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // uint256 gasEnd = gasleft();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+
+        // Assert
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance // + gasUsed
+        );
+    }
+
+    // Can we do our withdraw function a cheaper way?
+    function testWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), STARTING_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+        assert(
+            (numberOfFunders + 1) * SEND_VALUE ==
+                fundMe.getOwner().balance - startingOwnerBalance
+        );
+    }
+
+    // Test the cheaperWithdraw function
+    function testCheaperWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 2;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            hoax(address(i), STARTING_BALANCE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+    }
+
+    // Test the getPriceFeed function
+    function testGetPriceFeed() public {
+        AggregatorV3Interface priceFeed = fundMe.getPriceFeed();
+        assertEq(address(priceFeed), address(fundMe.getPriceFeed()));
+    }
+
+    // Test that multiple funding from the same address works correctly
+    function testMultipleFundingFromSameAddress() public {
+        vm.startPrank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        fundMe.fund{value: SEND_VALUE}();
+        fundMe.fund{value: SEND_VALUE}();
+        vm.stopPrank();
+
+        assertEq(fundMe.getAddressToAmountFunded(USER), SEND_VALUE * 3);
+    }
+
+    // Test that funding with exactly the minimum amount works
+    function testFundingWithExactMinimum() public {
+        // Instead of calculating the exact minimum, let's use a value we know will work
+        uint256 minimumUsd = fundMe.MINIMUM_USD();
+
+        // Get the current ETH/USD price
+        uint256 price = uint256(PriceConverter.getPrice(fundMe.getPriceFeed()));
+
+        // Calculate ETH amount needed and add a small buffer to ensure it's enough
+        uint256 ethAmount = ((minimumUsd * 1e18) / price) + 1e15; // Add a small buffer
+
+        vm.prank(USER);
+        fundMe.fund{value: ethAmount}();
+
+        assertEq(fundMe.getAddressToAmountFunded(USER), ethAmount);
     }
 }
