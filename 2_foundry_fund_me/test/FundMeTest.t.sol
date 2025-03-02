05fe4570391d8aea38ebf2593e6d29d4b2013bc0
shisukeUrahara <abhisheksagu@gmail.com>
2025-03-02 13:06:46 +0530

    added basic deployfundme script and fundme test file

---
+++ 2_foundry_fund_me/test/FundMeTest.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    // Create test addresses
+    address USER = makeAddr("user");
+    address ANOTHER_USER = makeAddr("anotherUser");
+
+    // Set initial balance for test users
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        // Deploy the contract directly instead of using DeployFundMe
+        fundMe = new FundMe();
+
+        // Fund test users with ETH
+        vm.deal(USER, STARTING_BALANCE);
+        vm.deal(ANOTHER_USER, STARTING_BALANCE);
+    }
+
+    // ==================== UNIT TESTS ====================
+
+    // Test if the minimum USD amount is set correctly
+    function testMinimumUsdIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    // Test if the owner is set correctly
+    function testOwnerIsDeployer() public view {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
