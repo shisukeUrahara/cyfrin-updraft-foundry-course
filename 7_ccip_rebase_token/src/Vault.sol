863bf48d52ab937799ecd216949478b64380a65e
shisukeUrahara <abhisheksagu@gmail.com>
2025-04-19 14:21:30 +0530

    added IRebaseToken and deposit function in Vault

--- 7_ccip_rebase_token/src/Vault.sol
+++ 7_ccip_rebase_token/src/Vault.sol
@@ -21,16 +21,28 @@ pragma solidity ^0.8.24;
 // internal
 // private
 // view & pure functions
+
+import {IRebaseToken} from "./Interfaces/IRebaseToken.sol";
 contract Vault {
     // ERRORS
-    error Vault__NotEnoughBalance();
+    error Vault__CannotDepositZero();
     // state variables
     address private immutable i_rebaseToken;
 
     // events
-
+    event Deposit(address indexed user, uint256 amount);
     constructor(address _rebaseToken) {
         i_rebaseToken = _rebaseToken;
+    }
+
+    receive() external payable {}
+
+    function deposit() external payable {
+        if (msg.value == 0) {
+            revert Vault__CannotDepositZero();
+        }
+        IRebaseToken(i_rebaseToken).mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
     }
 
     function getRebaseTokenAddress() external view returns (address) {
