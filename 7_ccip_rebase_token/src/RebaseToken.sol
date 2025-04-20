e42099028a6490305fc80959707ea1579666b9ea
shisukeUrahara <abhisheksagu@gmail.com>
2025-04-20 11:39:51 +0530

    updated precision factor for more accuracy

--- 7_ccip_rebase_token/src/RebaseToken.sol
+++ 7_ccip_rebase_token/src/RebaseToken.sol
@@ -39,12 +39,12 @@ contract RebaseToken is ERC20, Ownable, AccessControl {
     );
 
     // state variables
-    uint256 private s_interestRate = 5e10; // 1e18 ~1%
-    mapping(address => uint256) private s_userInterestRate;
-    mapping(address => uint256) private s_userLastUpdatedTimestamp;
-    uint256 private constant PRECISION_FACTOR = 1e18;
+    uint256 private constant PRECISION_FACTOR = 1e27;
     bytes32 public constant MINT_AND_BURN_ROLE =
         keccak256("MINT_AND_BURN_ROLE");
+    uint256 private s_interestRate = (5 * PRECISION_FACTOR) / 1e8; // 1e18 ~1%
+    mapping(address => uint256) private s_userInterestRate;
+    mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
     // events
     event InterestRateSet(uint256 newInterestRate);
